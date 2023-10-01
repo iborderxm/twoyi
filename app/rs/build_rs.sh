3238bc9999fd8afc755189d4969767df04827f91
iborder <iborder@139.com>
2023-10-01 08:22:45 +0800

    更新build_rs.sh

--- app/rs/build_rs.sh
+++ app/rs/build_rs.sh
@@ -5,5 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
-
-cargo xdk -t arm64-v8a -o ../src/main/jniLibs build $1 || true
+cargo install cargo-xdk
+rustup target add  aarch64-linux-android 
+cargo xdk -t arm64-v8a -o ../src/main/jniLibs build $1
+#cargo xdk -t arm64-v8a -o ../src/main/jniLibs build $1 || true
